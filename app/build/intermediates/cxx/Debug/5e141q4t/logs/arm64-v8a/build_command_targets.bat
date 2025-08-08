3e497bdd74d05d5ca59cb92b9f3f07516d8430fe
realmrhigh <stantonhigh@gmail.com>
2025-08-08 14:53:11 -0700

    build data

---
+++ app/build/intermediates/cxx/Debug/5e141q4t/logs/arm64-v8a/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Users\\stant\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\stant\\AndroidStudioProjects\\AAudacitee\\app\\.cxx\\Debug\\5e141q4t\\arm64-v8a" ^
+  audioapp ^
+  parametric_eq
---
+++ app/build/intermediates/cxx/Debug/5e141q4t/logs/x86_64/build_command_targets.bat
@@ -0,0 +1,6 @@
+@echo off
+"C:\\Users\\stant\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\stant\\AndroidStudioProjects\\AAudacitee\\app\\.cxx\\Debug\\5e141q4t\\x86_64" ^
+  audioapp ^
+  parametric_eq
